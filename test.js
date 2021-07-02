a14ff5caaf93d96fd53a4d1e29a9704116d6582a
wiz <hi.donggi@gmail.com>
2021-07-02 15:22:58 +0900

    test

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
-const dgram = require('dgram');
+//const dgram = require('dgram');
+//
+//const client = dgram.createSocket('udp4');
+//client.send('hello world', 50000);
 
-const client = dgram.createSocket('udp4');
-client.send('hello world', 50000);
+import * as dbConfig from './config/dbConfig.json'
+import { Pool, Client } from 'pg'
+
+const pool = new Pool(dbConfig)
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
