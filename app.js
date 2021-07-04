28ddc82a3038801aa42afbde65c29b4d4bf52473
dev_donggi <hi.donggi@gmail.com>
2021-07-04 20:28:19 +0900

    호스트 수정

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const dgram = require('dgram')
 const fastify = require('fastify')({ logger: false })
 
 fastify.get('/health', (request, reply) => '')
-fastify.listen(50001)
+fastify.listen(50001, '0.0.0.0')
 
 
-const server = dgram.createSocket('udp4');
-server.on('message', console.log);
-server.bind(50000);
+const server = dgram.createSocket('udp4')
+server.on('message', console.log)
+server.bind(50000)
